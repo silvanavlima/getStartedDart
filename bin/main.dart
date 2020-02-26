45f92c6ab164bc277c0669756ed7a14ba2b2045c
Silvana <silvana.vieira@hotmail.com>
2020-02-26 01:32:28 -0300

    variables and constants

--- bin/main.dart
+++ bin/main.dart
@@ -32,4 +32,46 @@ void main() {
 
   print(isBrowser || isInvisible);
   print(isBrowser && isInvisible);
+
+  //Function
+  sum(a, b) {
+    return a + b;
+  }
+
+  print(sum(5, 10));
+
+//funcao pra saber a diferença entre valores
+  var difference = (int a, int b) {
+    return a - b;
+  };
+  print(difference(20, 10));
+
+  //funcao abreviada
+  var product = (int a, int b) => a * b;
+  print(product(3, 3));
+
+  //Lists
+  var fruits = ['banana', 'pineapple', 'lemon'];
+  print(fruits.length);
+
+  fruits.forEach((fruit) => print(fruit));
+
+  var mappedFruits = fruits.map((fruit) => 'I love $fruit').toList();
+  print(mappedFruits[1]);
+
+  //Maps
+  var user = {"name": "Silvana", "age": 30, "isSubscriber": true};
+  print(user["name"]);
+
+  user.forEach((key, value) => print("key: $key, value: $value"));
+
+  //Runes
+  Runes input = new Runes('I \u2764 Dart');
+  print(new String.fromCharCodes(input));
+
+  //Symbol
+  Symbol simb1 = Symbol('input');
+  var simb2 = #input;
+  print(simb1);
+  print(simb2);
 }
